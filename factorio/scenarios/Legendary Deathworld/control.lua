1ecc424e0b098ca66f80bef9751ea5639605c135
Atraps003 <atraps003@gmail.com>
2025-08-17 14:37:15 -0500

    add reset command for admins with logging

--- factorio/scenarios/Legendary Deathworld/control.lua
+++ factorio/scenarios/Legendary Deathworld/control.lua
@@ -1,3 +1,11 @@
 local handler = require("event_handler") handler.add_lib(require("freeplay")) if script.active_mods["space-age"] then handler.add_lib(require("space-finish-script")) else 
   handler.add_lib(require("silo-script"))
 end
+
+commands.add_command("reset", "Resets map", function(command)
+    local player = game.get_player(command.player_index)
+	if player.admin == true then
+	reset()
+    log(player.name  .. ' used reset command ')
+	end
+end)
